8a526f0855b92216e113756894b8c1564f8d72a6
Santosh Kumar <santosh.achary0706@gmail.com>
2022-12-28 20:13:58 +0530

    Test Bench Code

---
+++ Day-5 SR Flip Flop/SRFFTB.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.12.2022 18:08:40
+// Design Name: 
+// Module Name: SRFFTB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SRFFTB();
+reg [1:0]sr;
+reg clk;
+wire q,qbar;
+
+SRFF ut(.sr(sr),.clk(clk),.q(q),.qbar(qbar));
+
+initial begin
+    clk = 0;
+        forever #10 clk = ~clk;
+end
+
+initial begin
+    sr = 2'b00;
+    #100
+    sr = 2'b01;
+    #100
+    sr = 2'b10;
+    #100
+    sr = 2'b11;
+    end
+endmodule
