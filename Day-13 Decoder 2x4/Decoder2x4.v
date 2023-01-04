7ee26e2d3f75c2ace97a2b297ea6b45c111eb293
Santosh Kumar <santosh.achary0706@gmail.com>
2023-01-04 19:35:39 +0530

    Design Code

---
+++ Day-13 Decoder 2x4/Decoder2x4.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.01.2023 23:08:12
+// Design Name: 
+// Module Name: Decoder2x4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder2x4(a,b,en,d1,d2,d3,d4);
+input a,b,en;
+output d1,d2,d3,d4;
+
+assign 
+    d4 = (en & ~a & ~b),
+    d3 = (en & ~a & b),
+    d2 = (en & a & ~b),
+    d1 = (en & a & b);
+    
+endmodule
